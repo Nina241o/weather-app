ba030e30f8c031dfa94f8b094d6de8e8cb313ee4
Nina <florinaseipp@gmail.com>
2021-10-31 01:04:22 +0200

    Implemented the unit conversion and updated some of the styling of the page.

--- src/app.js
+++ src/app.js
@@ -43,6 +43,8 @@ if (minutes < 10) {
 currentDate.innerHTML = `${day}, ${date} ${month} ${year}, ${hours}:${minutes} hrs`;
 
 function showAllCityData(response) {
+  celsiusTemperature = response.data.main.temp;
+
   let cityElement = document.querySelector("ul #city");
   cityElement.innerHTML = response.data.name;
 
@@ -81,6 +83,7 @@ function searchCity(city) {
 
   axios.get(apiUrl).then(showAllCityData);
 }
+
 function executeCitySubmission(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
@@ -107,4 +110,30 @@ searchForm.addEventListener("submit", executeCitySubmission);
 
 let currentLocationElement = document.querySelector("#current-location");
 currentLocationElement.addEventListener("click", getCurrentLocation);
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 searchCity("Berlin");
