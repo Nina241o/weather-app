e4fbbfc65f5ad70620cd24c2d3bf026b6ba07ca7
Nina <florinaseipp@gmail.com>
2021-11-04 17:15:42 +0100

    Removed Fahrenheit conversion from HTML and Javascript.

--- src/app.js
+++ src/app.js
@@ -168,19 +168,8 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(getLocation);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
@@ -211,14 +200,6 @@ searchInput.addEventListener("submit", executeCitySubmission);
 
 let currentLocationElement = document.querySelector("#current-location");
 currentLocationElement.addEventListener("click", getCurrentLocation);
-
-let celsiusTemperature = null;
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let poemButton = document.querySelector("#array-poem");
 poemButton.addEventListener("click", displayAPoem);
